9ddd1a26210f456d54794f4387ffc4f813c4369a
Seko_Hardik <hardik@sstechsystem.com>
2021-12-03 14:04:36 +0530

    Add project files.

---
+++ DesignExplorer.DataAccess/Function.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignExplorer.DataAccess
+{
+    public class Function
+    {
+        public void  SendEmail()
+        {
+            //return "Done";
+        }
+    }
+}
---
+++ DesignExplorer.Web/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using DesignExplorer.Web.Models;
+using System.Web.Mvc;
+
+namespace DesignExplorer.Web.Controllers
+{
+    [CheckAuthorization]
+    public abstract class BaseController : Controller
+    {
+        // GET: Base
+        public BaseController()
+        {
+        }
+        private SessionProfile _currentProfile;
+        public SessionProfile CurrentProfile
+        {
+            get
+            {
+                if (_currentProfile == null)
+                {
+                    _currentProfile = ProfileHelper.Profile;
+                }
+
+                return _currentProfile;
+            }
+        }
+    }
+}
